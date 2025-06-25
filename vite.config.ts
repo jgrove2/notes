b292a0279e9f73994ff4f0fd30156579c1ef0bff
Justin <26723005+jgrove2@users.noreply.github.com>
2025-06-24 22:47:06 -0400

    remove server

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,5 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   plugins: [tanstackStart({ target: "vercel" }), tsConfigPaths()],
 });
